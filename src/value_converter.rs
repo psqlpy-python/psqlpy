1a0f9fbb6144e89292f6908ea979e0d035c31d82
chandr-andr (Kiselev Aleksandr) <askiselev00@gmail.com>
2024-02-11 03:55:32 +0100

    Added support for JSONB and JSON types

--- src/value_converter.rs
+++ src/value_converter.rs
@@ -1,4 +1,5 @@
 use chrono::{self, DateTime, FixedOffset, NaiveDate, NaiveDateTime, NaiveTime};
+use serde_json::{json, Map, Value};
 use std::fmt::Debug;
 use uuid::Uuid;
 
@@ -6,8 +7,8 @@ use bytes::{BufMut, BytesMut};
 use postgres_protocol::types;
 use pyo3::{
     types::{
-        PyBool, PyBytes, PyDate, PyDateTime, PyFloat, PyInt, PyList, PySet, PyString, PyTime,
-        PyTuple,
+        PyBool, PyBytes, PyDate, PyDateTime, PyDict, PyFloat, PyInt, PyList, PySet, PyString,
+        PyTime, PyTuple,
     },
     Py, PyAny, Python, ToPyObject,
 };
@@ -37,6 +38,7 @@ pub enum PythonDTO {
     PyIntI32(i32),
     PyIntI64(i64),
     PyIntU32(u32),
+    PyIntU64(u64),
     PyFloat32(f32),
     PyFloat64(f64),
     PyDate(NaiveDate),
@@ -45,9 +47,9 @@ pub enum PythonDTO {
     PyDateTimeTz(DateTime<FixedOffset>),
     PyList(Vec<PythonDTO>),
     PyTuple(Vec<PythonDTO>),
+    PyDict(Value),
 }
 
-/// Implement necessary methods for this type.
 impl PythonDTO {
     /// Return type of the Array for PostgreSQL.
     ///
@@ -67,8 +69,34 @@ impl PythonDTO {
             PythonDTO::PyFloat32(_) => Ok(tokio_postgres::types::Type::FLOAT4_ARRAY),
             PythonDTO::PyFloat64(_) => Ok(tokio_postgres::types::Type::FLOAT8_ARRAY),
             PythonDTO::PyIntU32(_) => Ok(tokio_postgres::types::Type::INT4_ARRAY),
+            PythonDTO::PyDict(_) => Ok(tokio_postgres::types::Type::JSONB_ARRAY),
             _ => Err(RustPSQLDriverError::PyToRustValueConversionError(
                 "Can't process array type, your type doesn't have support yet".into(),
+            )),
+        }
+    }
+
+    pub fn to_serde_value(&self) -> RustPSQLDriverPyResult<Value> {
+        match self {
+            PythonDTO::PyNone => Ok(Value::Null),
+            PythonDTO::PyBool(pybool) => Ok(json!(pybool)),
+            PythonDTO::PyString(pystring) => Ok(json!(pystring)),
+            PythonDTO::PyIntI32(pyint) => Ok(json!(pyint)),
+            PythonDTO::PyIntI64(pyint) => Ok(json!(pyint)),
+            PythonDTO::PyIntU64(pyint) => Ok(json!(pyint)),
+            PythonDTO::PyFloat64(pyfloat) => Ok(json!(pyfloat)),
+            PythonDTO::PyList(pylist) => {
+                let mut vec_serde_values: Vec<Value> = vec![];
+
+                for py_object in pylist.iter() {
+                    vec_serde_values.push(py_object.to_serde_value()?);
+                }
+
+                return Ok(json!(vec_serde_values));
+            }
+            PythonDTO::PyDict(py_dict) => Ok(py_dict.clone()),
+            _ => Err(RustPSQLDriverError::PyToRustValueConversionError(
+                "Cannot convert data all your dict elements into Rust type".into(),
             )),
         }
     }
@@ -106,8 +134,9 @@ impl ToSql for PythonDTO {
     where
         Self: Sized,
     {
+        let mut return_is_null_true: bool = false;
         if *self == PythonDTO::PyNone {
-            return Ok(tokio_postgres::types::IsNull::Yes);
+            return_is_null_true = true;
         }
 
         match self {
@@ -126,6 +155,7 @@ impl ToSql for PythonDTO {
             PythonDTO::PyIntI32(int) => out.put_i32(*int),
             PythonDTO::PyIntI64(int) => out.put_i64(*int),
             PythonDTO::PyIntU32(int) => out.put_u32(*int),
+            PythonDTO::PyIntU64(int) => out.put_u64(*int),
             PythonDTO::PyFloat32(float) => out.put_f32(*float),
             PythonDTO::PyFloat64(float) => out.put_f64(*float),
             PythonDTO::PyDate(pydate) => {
@@ -145,10 +175,21 @@ impl ToSql for PythonDTO {
                 for inner in py_iterable.iter() {
                     items.push(inner);
                 }
-                items.to_sql(&items[0].array_type()?, out)?;
+                if items.len() > 1 {
+                    items.to_sql(&items[0].array_type()?, out)?;
+                } else {
+                    return_is_null_true = true;
+                }
+            }
+            PythonDTO::PyDict(py_dict) => {
+                <&Value as ToSql>::to_sql(&py_dict, ty, out)?;
             }
         }
-        Ok(tokio_postgres::types::IsNull::No)
+        if return_is_null_true {
+            return Ok(tokio_postgres::types::IsNull::Yes);
+        } else {
+            return Ok(tokio_postgres::types::IsNull::No);
+        }
     }
 
     to_sql_checked!();
@@ -270,6 +311,31 @@ pub fn py_to_rust(parameter: &PyAny) -> RustPSQLDriverPyResult<PythonDTO> {
         return Ok(PythonDTO::PyList(items));
     }
 
+    if parameter.is_instance_of::<PyDict>() {
+        let dict = parameter.downcast::<PyDict>().map_err(|error| {
+            RustPSQLDriverError::PyToRustValueConversionError(format!(
+                "Can't cast to inner dict: {error}"
+            ))
+        })?;
+
+        let mut serde_map: Map<String, Value> = Map::new();
+
+        for dict_item in dict.items() {
+            let py_list = dict_item.downcast::<PyTuple>().map_err(|error| {
+                RustPSQLDriverError::PyToRustValueConversionError(format!(
+                    "Cannot cast to list: {error}"
+                ))
+            })?;
+
+            let key = py_list.get_item(0)?.extract::<String>()?;
+            let value = py_to_rust(py_list.get_item(1)?)?;
+
+            serde_map.insert(key, value.to_serde_value()?);
+        }
+
+        return Ok(PythonDTO::PyDict(Value::Object(serde_map)));
+    }
+
     Err(RustPSQLDriverError::PyToRustValueConversionError(format!(
         "Can not covert you type {} into inner one",
         parameter,
@@ -387,137 +453,3 @@ pub fn postgres_to_py<'a>(
         )),
     }
 }
-
-// pub fn postgres_row_to_json_value(row: &Row) -> Result<String, Error> {
-//     let row_data = postgres_row_to_row_data(row)?;
-//     let a = JSONValue::Object(row_data);
-//     Ok(a.to_string())
-// }
-
-// // some type-aliases I use in my project
-// pub type JSONValue = serde_json::Value;
-// pub type RowData = Map<String, JSONValue>;
-// pub type Error = anyhow::Error; // from: https://github.com/dtolnay/anyhow
-
-// pub fn postgres_row_to_row_data(row: &Row) -> Result<RowData, Error> {
-//     let mut result: Map<String, JSONValue> = Map::new();
-//     for (i, column) in row.columns().iter().enumerate() {
-//         let name = column.name();
-//         let json_value = pg_cell_to_json_value(&row, column, i)?;
-//         result.insert(name.to_string(), json_value);
-//     }
-//     Ok(result)
-// }
-
-// pub fn pg_cell_to_json_value(
-//     row: &Row,
-//     column: &Column,
-//     column_i: usize,
-// ) -> Result<JSONValue, Error> {
-//     // let f64_to_json_number = |raw_val: f64| -> Result<JSONValue, Error> {
-//     //     let temp = serde_json::Number::from_f64(raw_val.into())
-//     //         .ok_or(anyhow::anyhow!("invalid json-float"))?;
-//     //     Ok(JSONValue::Number(temp))
-//     // };
-//     Ok(match *column.type_() {
-//         // single types
-//         Type::BOOL => get_basic(row, column, column_i, |a: bool| Ok(JSONValue::Bool(a)))?,
-//         Type::INT2 => get_basic(row, column, column_i, |a: i16| {
-//             Ok(JSONValue::Number(serde_json::Number::from(a)))
-//         })?,
-//         Type::INT4 => get_basic(row, column, column_i, |a: i32| {
-//             Ok(JSONValue::Number(serde_json::Number::from(a)))
-//         })?,
-//         Type::INT8 => get_basic(row, column, column_i, |a: i64| {
-//             Ok(JSONValue::Number(serde_json::Number::from(a)))
-//         })?,
-//         Type::TEXT | Type::VARCHAR => {
-//             get_basic(row, column, column_i, |a: String| Ok(JSONValue::String(a)))?
-//         }
-//         Type::JSON | Type::JSONB => get_basic(row, column, column_i, |a: JSONValue| Ok(a))?,
-//         // Type::FLOAT4 => get_basic(row, column, column_i, |a: f32| {
-//         //     Ok(f64_to_json_number(a.into())?)
-//         // })?,
-//         // Type::FLOAT8 => get_basic(row, column, column_i, |a: f64| Ok(f64_to_json_number(a)?))?,
-//         // these types require a custom StringCollector struct as an intermediary (see struct at bottom)
-//         Type::TS_VECTOR => get_basic(row, column, column_i, |a: StringCollector| {
-//             Ok(JSONValue::String(a.0))
-//         })?,
-
-//         // array types
-//         Type::BOOL_ARRAY => get_array(row, column, column_i, |a: bool| Ok(JSONValue::Bool(a)))?,
-//         Type::INT2_ARRAY => get_array(row, column, column_i, |a: i16| {
-//             Ok(JSONValue::Number(serde_json::Number::from(a)))
-//         })?,
-//         Type::INT4_ARRAY => get_array(row, column, column_i, |a: i32| {
-//             Ok(JSONValue::Number(serde_json::Number::from(a)))
-//         })?,
-//         Type::INT8_ARRAY => get_array(row, column, column_i, |a: i64| {
-//             Ok(JSONValue::Number(serde_json::Number::from(a)))
-//         })?,
-//         Type::TEXT_ARRAY | Type::VARCHAR_ARRAY => {
-//             get_array(row, column, column_i, |a: String| Ok(JSONValue::String(a)))?
-//         }
-//         Type::JSON_ARRAY | Type::JSONB_ARRAY => {
-//             get_array(row, column, column_i, |a: JSONValue| Ok(a))?
-//         }
-//         // Type::FLOAT4_ARRAY => get_array(row, column, column_i, |a: f32| {
-//         //     Ok(f64_to_json_number(a.into())?)
-//         // })?,
-//         // Type::FLOAT8_ARRAY => {
-//         //     get_array(row, column, column_i, |a: f64| Ok(f64_to_json_number(a)?))?
-//         // }
-//         // these types require a custom StringCollector struct as an intermediary (see struct at bottom)
-//         Type::TS_VECTOR_ARRAY => get_array(row, column, column_i, |a: StringCollector| {
-//             Ok(JSONValue::String(a.0))
-//         })?,
-
-//         _ => anyhow::bail!(
-//             "Cannot convert pg-cell \"{}\" of type \"{}\" to a JSONValue.",
-//             column.name(),
-//             column.type_().name()
-//         ),
-//     })
-// }
-
-// fn get_basic<'a, T: FromSql<'a>>(
-//     row: &'a Row,
-//     column: &Column,
-//     column_i: usize,
-//     val_to_json_val: impl Fn(T) -> Result<JSONValue, Error>,
-// ) -> T {
-//     let rust_type = row.try_get::<_, Option<T>>(column_i).unwrap().unwrap();
-// }
-// fn get_array<'a, T: FromSql<'a>>(
-//     row: &'a Row,
-//     column: &Column,
-//     column_i: usize,
-//     val_to_json_val: impl Fn(T) -> Result<JSONValue, Error>,
-// ) -> Result<JSONValue, Error> {
-//     let raw_val_array = row.try_get::<_, Option<Vec<T>>>(column_i)?;
-//     Ok(match raw_val_array {
-//         Some(val_array) => {
-//             let mut result = vec![];
-//             for val in val_array {
-//                 result.push(val_to_json_val(val)?);
-//             }
-//             JSONValue::Array(result)
-//         }
-//         None => JSONValue::Null,
-//     })
-// }
-
-// // you can remove this section if not using TS_VECTOR (or other types requiring an intermediary `FromSQL` struct)
-// struct StringCollector(String);
-// impl FromSql<'_> for StringCollector {
-//     fn from_sql(
-//         _: &Type,
-//         raw: &[u8],
-//     ) -> Result<StringCollector, Box<dyn std::error::Error + Sync + Send>> {
-//         let result = std::str::from_utf8(raw)?;
-//         Ok(StringCollector(result.to_owned()))
-//     }
-//     fn accepts(_ty: &Type) -> bool {
-//         true
-//     }
-// }
