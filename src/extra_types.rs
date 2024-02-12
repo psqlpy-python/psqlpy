95b36f4751af0068fd140cb3357b2d6db3dad76f
chandr-andr (Kiselev Aleksandr) <askiselev00@gmail.com>
2024-02-12 22:07:55 +0100

    Moved buildserdevalue into value_converter

--- src/extra_types.rs
+++ src/extra_types.rs
@@ -1,17 +1,10 @@
 use std::str::FromStr;
 
-use pyo3::{
-    pyclass, pymethods,
-    types::{PyDict, PyList, PyModule},
-    PyAny, PyResult, Python,
-};
-use serde_json::{json, Value};
+use pyo3::{pyclass, pymethods, types::PyModule, PyAny, PyResult, Python};
+use serde_json::Value;
 use uuid::Uuid;
 
-use crate::{
-    exceptions::rust_errors::{RustPSQLDriverError, RustPSQLDriverPyResult},
-    value_converter::py_to_rust,
-};
+use crate::{exceptions::rust_errors::RustPSQLDriverPyResult, value_converter::build_serde_value};
 
 macro_rules! build_python_type {
     ($st_name:ident, $rust_type:ty) => {
@@ -64,35 +57,6 @@ impl PyUUID {
         Ok(Self {
             inner: Uuid::from_str(&uuid_value)?,
         })
-    }
-}
-
-pub fn build_serde_value<'a>(value: &'a PyAny) -> RustPSQLDriverPyResult<Value> {
-    if value.is_instance_of::<PyList>() {
-        let mut result_vec: Vec<Value> = vec![];
-
-        let params: Vec<&PyAny> = value.extract::<Vec<&PyAny>>()?;
-
-        for inner in params.iter() {
-            if inner.is_instance_of::<PyDict>() {
-                let python_dto = py_to_rust(inner)?;
-                result_vec.push(python_dto.to_serde_value()?);
-            } else if inner.is_instance_of::<PyList>() {
-                let serde_value = build_serde_value(inner)?;
-                result_vec.push(serde_value);
-            } else {
-                return Err(RustPSQLDriverError::PyToRustValueConversionError(format!(
-                    "PyJSON/PyJSONB supports only list of lists or list of dicts."
-                )));
-            }
-        }
-        return Ok(json!(result_vec));
-    } else if value.is_instance_of::<PyDict>() {
-        return py_to_rust(value)?.to_serde_value();
-    } else {
-        return Err(RustPSQLDriverError::PyToRustValueConversionError(format!(
-            "PyJSON must be list value."
-        )));
     }
 }
 
--- src/value_converter.rs
+++ src/value_converter.rs
@@ -469,3 +469,32 @@ pub fn postgres_to_py<'a>(
         )),
     }
 }
+
+pub fn build_serde_value<'a>(value: &'a PyAny) -> RustPSQLDriverPyResult<Value> {
+    if value.is_instance_of::<PyList>() {
+        let mut result_vec: Vec<Value> = vec![];
+
+        let params: Vec<&PyAny> = value.extract::<Vec<&PyAny>>()?;
+
+        for inner in params.iter() {
+            if inner.is_instance_of::<PyDict>() {
+                let python_dto = py_to_rust(inner)?;
+                result_vec.push(python_dto.to_serde_value()?);
+            } else if inner.is_instance_of::<PyList>() {
+                let serde_value = build_serde_value(inner)?;
+                result_vec.push(serde_value);
+            } else {
+                return Err(RustPSQLDriverError::PyToRustValueConversionError(format!(
+                    "PyJSON/PyJSONB supports only list of lists or list of dicts."
+                )));
+            }
+        }
+        return Ok(json!(result_vec));
+    } else if value.is_instance_of::<PyDict>() {
+        return py_to_rust(value)?.to_serde_value();
+    } else {
+        return Err(RustPSQLDriverError::PyToRustValueConversionError(format!(
+            "PyJSON must be list value."
+        )));
+    }
+}
