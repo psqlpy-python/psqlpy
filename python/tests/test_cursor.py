67e5a4744826efed0dc87bba550909c782bf5ba7
chandr-andr (Kiselev Aleksandr) <askiselev00@gmail.com>
2024-02-28 13:31:23 +0400

    Continue adding tests

---
+++ python/tests/test_cursor.py
@@ -0,0 +1,16 @@
+import pytest
+
+from psqlpy import PSQLPool
+
+
+@pytest.mark.anyio
+async def test_cursor_fetch(
+    psql_pool: PSQLPool,
+    table_name: str,
+    number_database_records: int,
+) -> None:
+    connection = await psql_pool.connection()
+    transaction = connection.transaction()
+    await transaction.cursor(
+        querystring=f"SELECT * FROM {table_name}",
+    )
