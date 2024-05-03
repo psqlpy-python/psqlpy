63ce72380923bd3b618b5794da897826b1e59c4d
chandr-andr (Kiselev Aleksandr) <chandr@chandr.net>
2024-05-03 17:29:22 +0200

    Added fetch method, changed parameters annotations. Added tests
    Signed-off-by: chandr-andr (Kiselev Aleksandr) <chandr@chandr.net>

--- python/tests/test_connection_pool.py
+++ python/tests/test_connection_pool.py
@@ -49,6 +49,23 @@ async def test_pool_execute(
     assert len(inner_result) == number_database_records
 
 
+async def test_pool_fetch(
+    psql_pool: ConnectionPool,
+    table_name: str,
+    number_database_records: int,
+) -> None:
+    """Test that ConnectionPool can fetch queries."""
+    select_result = await psql_pool.fetch(
+        f"SELECT * FROM {table_name}",
+    )
+
+    assert type(select_result) == QueryResult
+
+    inner_result = select_result.result()
+    assert isinstance(inner_result, list)
+    assert len(inner_result) == number_database_records
+
+
 async def test_pool_connection(
     psql_pool: ConnectionPool,
 ) -> None:
