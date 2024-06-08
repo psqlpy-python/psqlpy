467f535a56c56dc627bce1ced1b0c8da763a839b
chandr-andr (Kiselev Aleksandr) <chandr@chandr.net>
2024-06-08 22:33:32 +0200

    Added support for FLOAT4/FLOAT8 in PostgreSQL
    Signed-off-by: chandr-andr (Kiselev Aleksandr) <chandr@chandr.net>

--- python/tests/test_value_converter.py
+++ python/tests/test_value_converter.py
@@ -11,6 +11,8 @@ from psqlpy import ConnectionPool
 from psqlpy._internal.extra_types import PyCustomType
 from psqlpy.extra_types import (
     BigInt,
+    Float32,
+    Float64,
     Integer,
     PyJSON,
     PyJSONB,
@@ -74,6 +76,8 @@ async def test_as_class(
         ("INT4", Integer(121231231), 121231231),
         ("INT8", BigInt(99999999999999999), 99999999999999999),
         ("FLOAT4", 32.12329864501953, 32.12329864501953),
+        ("FLOAT4", Float32(32.12329864501953), 32.12329864501953),
+        ("FLOAT8", Float64(32.12329864501953), 32.12329864501953),
         ("DATE", now_datetime.date(), now_datetime.date()),
         ("TIME", now_datetime.time(), now_datetime.time()),
         ("TIMESTAMP", now_datetime, now_datetime),
@@ -288,6 +292,7 @@ async def test_deserialization_composite_into_python(
         int4_ INT4,
         int8_ INT8,
         flaot4_ FLOAT4,
+        flaot8_ FLOAT8,
         date_ DATE,
         time_ TIME,
         timestamp_ TIMESTAMP,
@@ -325,7 +330,7 @@ async def test_deserialization_composite_into_python(
         querystring=create_table_query,
     )
     await psql_pool.execute(
-        querystring="INSERT INTO for_test VALUES (ROW($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31))",  # noqa: E501
+        querystring="INSERT INTO for_test VALUES (ROW($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32))",  # noqa: E501
         parameters=[
             b"Bytes",
             "Some String",
@@ -335,6 +340,7 @@ async def test_deserialization_composite_into_python(
             Integer(199),
             BigInt(10001),
             32.12329864501953,
+            Float64(32.12329864501953),
             now_datetime.date(),
             now_datetime.time(),
             now_datetime,
@@ -400,6 +406,7 @@ async def test_deserialization_composite_into_python(
         int4_: int
         int8_: int
         flaot4_: float
+        flaot8_: float
         date_: datetime.date
         time_: datetime.time
         timestamp_: datetime.datetime
