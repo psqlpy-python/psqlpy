331e643083f57680dd24fecdb2b9f5d0926de301
chandr-andr (Kiselev Aleksandr) <chandr@chandr.net>
2024-06-24 21:54:41 +0200

    Added predefined row factories
    Signed-off-by: chandr-andr (Kiselev Aleksandr) <chandr@chandr.net>

---
+++ python/psqlpy/_internal/row_factories.pyi
@@ -0,0 +1,46 @@
+from typing import Any, Generic, Tuple, Type, TypeVar
+
+from typing_extensions import Self
+
+_CustomClass = TypeVar(
+    "_CustomClass",
+)
+
+def tuple_row(row: dict[str, Any]) -> Tuple[Tuple[str, Any]]:
+    """Convert dict row into tuple row.
+
+    ### Parameters:
+    - `row`: row in dictionary.
+
+    ### Returns:
+    row as a tuple of tuples.
+
+    ### Example:
+    ```
+    dict_ = {
+        "psqlpy": "is",
+        "postgresql": "driver",
+    }
+    # This function will convert this dict into:
+    (("psqlpy", "is"), ("postgresql": "driver"))
+    ```
+    """
+
+class class_row(Generic[_CustomClass]):  # noqa: N801
+    """Row converter to specified class."""
+
+    def __init__(self: Self, class_: Type[_CustomClass]) -> None:
+        """Construct new `class_row`.
+
+        ### Parameters:
+        - `class_`: class to transform row into.
+        """
+    def __call__(self, row: dict[str, Any]) -> _CustomClass:
+        """Convert row into specified class.
+
+        ### Parameters:
+        - `row`: row in dictionary.
+
+        ### Returns:
+        Constructed specified class.
+        """
