e64a6c80f5b122ce5802e385c20a2f1006804a93
chandr-andr (Kiselev Aleksandr) <chandr@chandr.net>
2024-06-26 20:24:51 +0200

    Added tests for predefined row factories
    Signed-off-by: chandr-andr (Kiselev Aleksandr) <chandr@chandr.net>

--- python/psqlpy/_internal/row_factories.pyi
+++ python/psqlpy/_internal/row_factories.pyi
@@ -27,7 +27,28 @@ def tuple_row(row: dict[str, Any]) -> Tuple[Tuple[str, Any]]:
     """
 
 class class_row(Generic[_CustomClass]):  # noqa: N801
-    """Row converter to specified class."""
+    """Row converter to specified class.
+
+    ### Example:
+    ```python
+    from psqlpy.row_factories import class_row
+
+
+    class ValidationModel:
+        name: str
+        views_count: int
+
+
+    async def main:
+        res = await db_pool.execute(
+            "SELECT * FROM users",
+        )
+
+        results: list[ValidationModel] = res.row_factory(
+            class_row(ValidationModel),
+        )
+    ```
+    """
 
     def __init__(self: Self, class_: Type[_CustomClass]) -> None:
         """Construct new `class_row`.
