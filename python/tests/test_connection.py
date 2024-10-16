22a569440d52be2d8379795380b0ab19568ec0dd
chandr-andr (Kiselev Aleksandr) <chandr@chandr.net>
2024-10-16 22:17:29 +0200

    Added Python 3.13 support
    Signed-off-by: chandr-andr (Kiselev Aleksandr) <chandr@chandr.net>

--- python/tests/test_connection.py
+++ python/tests/test_connection.py
@@ -1,12 +1,8 @@
 from __future__ import annotations
 
-import os
 import typing
-from io import BytesIO
 
 import pytest
-from pgpq import ArrowToPostgresBinaryEncoder
-from pyarrow import parquet
 from tests.helpers import count_rows_in_test_table
 
 from psqlpy import ConnectionPool, Cursor, QueryResult, Transaction
@@ -184,58 +180,6 @@ async def test_closed_connection_error(
 
     with pytest.raises(expected_exception=ConnectionClosedError):
         await connection.execute("SELECT 1")
-
-
-async def test_binary_copy_to_table(
-    psql_pool: ConnectionPool,
-) -> None:
-    """Test binary copy in connection."""
-    table_name: typing.Final = "cars"
-    await psql_pool.execute(f"DROP TABLE IF EXISTS {table_name}")
-    await psql_pool.execute(
-        """
-CREATE TABLE IF NOT EXISTS cars (
-    model VARCHAR,
-    mpg FLOAT8,
-    cyl INTEGER,
-    disp FLOAT8,
-    hp INTEGER,
-    drat FLOAT8,
-    wt FLOAT8,
-    qsec FLOAT8,
-    vs INTEGER,
-    am INTEGER,
-    gear INTEGER,
-    carb INTEGER
-);
-""",
-    )
-
-    arrow_table = parquet.read_table(
-        f"{os.path.dirname(os.path.abspath(__file__))}/test_data/MTcars.parquet",  # noqa: PTH120, PTH100
-    )
-    encoder = ArrowToPostgresBinaryEncoder(arrow_table.schema)
-    buf = BytesIO()
-    buf.write(encoder.write_header())
-    for batch in arrow_table.to_batches():
-        buf.write(encoder.write_batch(batch))
-    buf.write(encoder.finish())
-    buf.seek(0)
-
-    async with psql_pool.acquire() as connection:
-        inserted_rows = await connection.binary_copy_to_table(
-            source=buf,
-            table_name=table_name,
-        )
-
-    expected_inserted_row: typing.Final = 32
-
-    assert inserted_rows == expected_inserted_row
-
-    real_table_rows: typing.Final = await psql_pool.execute(
-        f"SELECT COUNT(*) AS rows_count FROM {table_name}",
-    )
-    assert real_table_rows.result()[0]["rows_count"] == expected_inserted_row
 
 
 async def test_execute_batch_method(psql_pool: ConnectionPool) -> None:
